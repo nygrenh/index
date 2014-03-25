ece5a614a643a7bdbe070361d628ba632448200f
Henrik Nygren <nygrenh@gmail.com>
2014-03-25 22:20:45 +0200

    Cleaned up links page

--- spec/views/links/index.html.erb_spec.rb
+++ spec/views/links/index.html.erb_spec.rb
@@ -21,15 +21,4 @@ describe "links/index" do
       )
     ])
   end
-
-  it "renders a list of links" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "Url".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Source".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "Domain".to_s, :count => 2
-  end
 end
