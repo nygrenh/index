1b2c78a0712357001285360741b28e62f88a43b7
Henrik Nygren <nygrenh@gmail.com>
2014-03-25 19:48:41 +0200

    An attempt to get tests to pass on Travis

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Index::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+if Rails.env.test? 
+	Index::Application.config.secret_key_base = 'f9610bdd1e99698872dda321217908adffc7771d3806dd60f4630b43c96eb142baffdc6bbe703539529aa6ed9f996c4dc606f26944bbee5f04149de214735d3f'
+else
+	Index::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+end
