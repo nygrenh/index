a96d5003d1740eb86b7e2051d3ca92186dd97b89
Henrik Nygren <nygrenh@gmail.com>
2014-04-09 22:11:24 +0300

    Added markdown support to tags and domains

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 	def markdown(text)
+		return if text.nil? 
 		renderer = Redcarpet::Render::HTML.new(filter_html: true)
 		markdown = Redcarpet::Markdown.new(renderer, autolink: true)
 		raw markdown.render(text)
