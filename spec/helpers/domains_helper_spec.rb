8f272c0329ec10f97f7d5417c1603b53f7a1be41
Henrik Nygren <nygrenh@gmail.com>
2014-04-22 22:20:00 +0300

    Cleaned pending tests

--- spec/helpers/domains_helper_spec.rb
+++ spec/helpers/domains_helper_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the DomainsHelper. For example:
-#
-# describe DomainsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe DomainsHelper do
 end
--- spec/models/link_tag_spec.rb
+++ spec/models/link_tag_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe LinkTag do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
