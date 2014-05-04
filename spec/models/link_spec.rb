1b3dfd1c4cf8020701376a59d229e8be00e779c7
Henrik Nygren <nygrenh@gmail.com>
2014-05-04 22:53:19 +0300

    Added tests for link's validations

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Link do
+  it "isn't valid without url" do 
+    link = Link.create
+    expect(link).not_to be_valid
+  end
+
+  it "isn't valid with an invalid url" do 
+    link = Link.create url:"invalid"
+    expect(link).not_to be_valid
+  end
 end
