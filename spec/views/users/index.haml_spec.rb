159878edfb6f883697977a35de8a01ea34f0e381
Henrik Nygren <nygrenh@gmail.com>
2014-05-19 21:27:15 +0300

    Cleaned up user list

--- spec/views/users/index.haml_spec.rb
+++ spec/views/users/index.haml_spec.rb
@@ -16,7 +16,6 @@ describe "users/index.haml" do
 
   it "renders a list of users" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "ul>li", :text => "Name".to_s, :count => 2
   end
 end
