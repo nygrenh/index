085df94223593acd8b6f16a9e8ee1fc18b554db6
Henrik Nygren <nygrenh@gmail.com>
2014-06-30 23:26:39 +0300

    Refactored link model spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
-	factory :user do
-		name 'Admin'
-		password 'password'
-		password_confirmation 'password'
-	end
+  factory :user do
+    name 'Admin'
+    password 'password'
+    password_confirmation 'password'
+  end
+
+  factory :link do
+  	url 'http://www.example.com/'
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 describe Link do
-  it "isn't valid without url" do 
-    link = Link.create
-    expect(link).not_to be_valid
+  let(:link)  { FactoryGirl.build(:link) }
+  it { expect(link).to be_valid }
+
+  context 'without url' do
+    let(:link)  { FactoryGirl.build(:link, url: nil) }
+    it { expect(link).not_to be_valid }
   end
 
-  it "isn't valid with an invalid url" do 
-    link = Link.create url:"invalid"
-    expect(link).not_to be_valid
-  end
-
-  it "is valid with a valid url" do
-    link = Link.create url:"https://github.com"
-    expect(link).to be_valid
+  context 'with an invalid url' do
+    let(:link)  { FactoryGirl.build(:link, url: 'invalid') }
+    it { expect(link).not_to be_valid }
   end
 end
