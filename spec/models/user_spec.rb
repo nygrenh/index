77a157f9561c86cb8b80b5366f538e1cd89788a3
Henrik Nygren <nygrenh@gmail.com>
2014-07-01 00:07:11 +0300

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 describe User do
-  it "should have a unique name" do 
-    User.create name:"admin", password:"a", password_confirmation:"a"
-    user = User.create name:"admin", password:"a", password_confirmation:"a"
-    expect(user).not_to be_valid
+  let(:user)  { FactoryGirl.build(:user) }
+  it { expect(user).to be_valid }
+
+  context 'without name' do
+    let(:user)  { FactoryGirl.build(:user, name: nil) }
+    it { expect(user).not_to be_valid }
   end
+
+  context 'without unique name' do
+    let(:user2)  { FactoryGirl.create(:user) }
+    it 'should not be valid' do
+      expect(user2).to be_valid
+      expect(user).not_to be_valid
+    end
+  end
+
+  context 'without password' do
+    let(:user)  { FactoryGirl.build(:user, password: nil) }
+    it { expect(user).not_to be_valid }
+  end
+
 end
