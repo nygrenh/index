42235b3eb05c884d5fa0a201eb9f6115f679f7a4
Henrik Nygren <nygrenh@gmail.com>
2014-07-10 14:52:35 +0300

    Show timestamps under links

--- spec/views/shared/_links.haml_spec.rb
+++ spec/views/shared/_links.haml_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe 'shared/_links.haml' do
-  let(:links) { [FactoryGirl.create(:link, title: 'link_title')] }
+  subject(:rendered) { render template: 'shared/_links.haml', locals: { links: links } }
+  let(:link) { FactoryGirl.create(:link, title: 'link_title') }
+  let(:links) { [link] }
+  let(:time) { Time.utc(2014, 07, 9, 5) }
+  let(:diff) { 7.minutes }
+
   it 'renders the link' do
-    render template: 'shared/_links.haml', locals: { links: links }
     expect(rendered).to match(links.first.title)
+  end
+
+  it 'shows a timestamp' do
+    allow(Time).to receive(:now) { time }
+    allow(link).to receive(:created_at) { time - diff }
+    expect(rendered).to match(/7 minutes ago/)
   end
 end
