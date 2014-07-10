47def7f4836cb2bdcf3f4617a60ea40a78df0bbb
Henrik Nygren <nygrenh@gmail.com>
2014-07-10 15:13:06 +0300

    Fix a broken spec

--- spec/views/links/index.haml_spec.rb
+++ spec/views/links/index.haml_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 describe 'links/index.haml' do
   subject(:rendered) { render }
   before(:each) do
-    links = [
-      stub_model(Link,
-                 title: 'Title',
-                 url: 'Url',
-                 description: 'MyText',
-                 source: 'Source',
-                 user_id: 1,
-                 domain_id: 'Domain'
-      )
-    ]
+    link = stub_model(Link,
+                      title: 'Title',
+                      url: 'Url',
+                      description: 'MyText',
+                      source: 'Source',
+                      user_id: 1,
+                      domain_id: 'Domain'
+    )
+    links = [link]
     allow(links).to receive(:current_page) { 1 }
     allow(links).to receive(:total_pages) { 1 }
     allow(links).to receive(:limit_value) { 1 }
+    allow(link).to receive(:created_at) { Time.now }
     assign(:links, links)
   end
 
