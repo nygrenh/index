f93d9a57073fe1929f1b993c42614bc04dc85598
Henrik Nygren <nygrenh@gmail.com>
2014-07-12 22:23:04 +0300

    Refactored domains_controller spec

--- spec/controllers/domains_controller_spec.rb
+++ spec/controllers/domains_controller_spec.rb
@@ -2,85 +2,70 @@ require 'rails_helper'
 
 describe DomainsController do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Domain. As you add validations to Domain, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) { { :domain => "", "user_id" => "1" } }
+  let(:user) { FactoryGirl.create(:user) }
+  let!(:domain) { FactoryGirl.create(:domain, user_id: user.id) }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # DomainsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { { user_id: user.id } }
 
-  before :each do
-    user = User.create name:"Test", password:"test", password_confirmation:"test"
-    session[:user_id] = user.id
-  end
-
-  describe "GET index" do
-    it "assigns all domains as @domains" do
-      domain = Domain.create! valid_attributes
+  describe 'GET index' do
+    it 'assigns all domains as @domains' do
       get :index, {}, valid_session
       expect(assigns(:domains)).to eq([domain])
     end
   end
 
-  describe "GET show" do
-    it "assigns the requested domain as @domain" do
-      domain = Domain.create! valid_attributes
-      get :show, {:id => domain.to_param}, valid_session
+  describe 'GET show' do
+    it 'assigns the requested domain as @domain' do
+      get :show, { id: domain.to_param }, valid_session
       expect(assigns(:domain)).to eq(domain)
     end
   end
 
-  describe "GET edit" do
-    it "assigns the requested domain as @domain" do
-      domain = Domain.create! valid_attributes
-      get :edit, {:id => domain.to_param}, valid_session
+  describe 'GET edit' do
+    it 'assigns the requested domain as @domain' do
+      get :edit, { id: domain.to_param }, valid_session
       expect(assigns(:domain)).to eq(domain)
     end
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested domain" do
-        domain = Domain.create! valid_attributes
+  describe 'PUT update' do
+    describe 'with valid params' do
+      it 'updates the requested domain' do
         # Assuming there are no other domains in the database, this
-        # specifies that the Domain created on the previous line
+        # specifies that the Domain created on the 'let!' above
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        expect_any_instance_of(Domain).to receive(:update).with({ "description" => "MyString" })
-        put :update, {:id => domain.to_param, :domain => { "description" => "MyString" }}, valid_session
+        expect_any_instance_of(Domain).to receive(:update).with('description' => 'MyString')
+        put :update, { id: domain.to_param, domain: { 'description' => 'MyString' } }, valid_session
       end
 
-      it "assigns the requested domain as @domain" do
-        domain = Domain.create! valid_attributes
-        put :update, {:id => domain.to_param, :domain => valid_attributes}, valid_session
+      it 'assigns the requested domain as @domain' do
+        put :update, { id: domain.to_param, domain: FactoryGirl.attributes_for(:domain) }, valid_session
         expect(assigns(:domain)).to eq(domain)
       end
 
-      it "redirects to the domain" do
-        domain = Domain.create! valid_attributes
-        put :update, {:id => domain.to_param, :domain => valid_attributes}, valid_session
+      it 'redirects to the domain' do
+        put :update, { id: domain.to_param, domain: FactoryGirl.attributes_for(:domain) }, valid_session
         expect(response).to redirect_to(domain)
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the domain as @domain" do
-        domain = Domain.create! valid_attributes
+    describe 'with invalid params' do
+      it 'assigns the domain as @domain' do
         # Trigger the behavior that occurs when invalid params are submitted
         allow_any_instance_of(Domain).to receive(:save).and_return(false)
-        put :update, {:id => domain.to_param, :domain => { "domain" => "invalid value" }}, valid_session
+        put :update, { id: domain.to_param, domain: { 'domain' => 'invalid value' } }, valid_session
         expect(assigns(:domain)).to eq(domain)
       end
 
-      it "re-renders the 'edit' template" do
-        domain = Domain.create! valid_attributes
+      it 're-renders the "edit" template' do
         # Trigger the behavior that occurs when invalid params are submitted
         allow_any_instance_of(Domain).to receive(:save).and_return(false)
-        put :update, {:id => domain.to_param, :domain => { "domain" => "invalid value" }}, valid_session
-        expect(response).to render_template("edit")
+        put :update, { id: domain.to_param, domain: { 'domain' => 'invalid value' } }, valid_session
+        expect(response).to render_template('edit')
       end
     end
   end
