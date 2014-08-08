4fe04c6a878116a8bd340695dbba5d3863894663
Henrik Nygren <nygrenh@gmail.com>
2014-08-08 19:27:08 +0300

    Allow Better Errors to be used with fig

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Index::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Allow dangerous debug stuff
+  BetterErrors::Middleware.allow_ip! "172.17.42.1/1"
 end
