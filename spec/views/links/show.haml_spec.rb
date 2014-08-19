d48f25f303899d6187d4c3c2ab9a200f71f7955d
Henrik Nygren <nygrenh@gmail.com>
2014-08-19 18:06:46 +0300

    Render tags on show link page

--- spec/views/links/show.haml_spec.rb
+++ spec/views/links/show.haml_spec.rb
@@ -1,23 +1,32 @@
 require 'rails_helper'
 
-describe "links/show.haml" do
+describe 'links/show.haml' do
+  let(:tag) { FactoryGirl.create(:tag) }
+
   before(:each) do
     FactoryGirl.build(:domain, domain: 'test.com')
     @link = assign(:link, stub_model(Link,
-      :title => "Title",
-      :url => "Url",
-      :description => "MyText",
-      :source => "Source",
-      :user_id => 1,
-      :domain_id => "1"
+      title: 'Title',
+      url: 'Url',
+      description: 'MyText',
+      source: 'Source',
+      user_id: 1,
+      domain_id: '1'
     ))
+    tags = [tag]
+    allow(@link).to receive(:tags) { tags }
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Url/)
     expect(rendered).to match(/MyText/)
+  end
+
+  it 'renders tags' do
+    render
+    expect(rendered).not_to match(tag.name)
   end
 end
