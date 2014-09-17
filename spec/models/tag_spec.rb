649a360b7e87ae306ce2052b7d1517af15541b28
Henrik Nygren <nygrenh@gmail.com>
2014-09-17 20:46:24 +0300

    Add a test for tag.update_link_count

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,15 @@ describe Tag do
   it { is_expected.to have_many(:notes) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:name) }
+
+  describe '.update_link_count' do
+    let(:tag) { FactoryGirl.create(:tag) }
+    let(:link) { FactoryGirl.create(:link) }
+    it 'updates the link count' do
+      tag.links << link
+      expect(tag.link_count).to eq(0)
+      tag.update_link_count
+      expect(tag.link_count).to eq(1)
+    end
+  end
 end
