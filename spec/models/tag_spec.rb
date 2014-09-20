3bb6153230496d37b9ee2f4149ff899540ab34d0
Henrik Nygren <nygrenh@gmail.com>
2014-09-20 22:26:45 +0300

    Add a test for Tag.get

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,19 +1,27 @@
 require 'rails_helper'
 
 describe Tag do
+  subject(:tag) { FactoryGirl.create(:tag) }
   it { is_expected.to have_many(:links) }
   it { is_expected.to have_many(:notes) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:name) }
 
   describe '.update_link_count' do
-    let(:tag) { FactoryGirl.create(:tag) }
     let(:link) { FactoryGirl.create(:link) }
     it 'updates the link count' do
       tag.links << link
       expect(tag.link_count).to eq(0)
       tag.update_link_count
       expect(tag.link_count).to eq(1)
+    end
+  end
+
+  describe '#get' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:tag) { FactoryGirl.create(:tag, user_id: user.id) }
+    it 'it should find tags case insensitively' do
+      expect(Tag.get(tag.name.upcase, user.id)).to eq tag
     end
   end
 end
