3d5d5f78a12c2ae5e00a3ffe7c36cf1ee87aeeae
Henrik Nygren <nygrenh@gmail.com>
2014-09-27 22:45:53 +0300

    Add link count to show domain page

--- spec/views/domains/show.haml_spec.rb
+++ spec/views/domains/show.haml_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
-describe "domains/show.haml" do
+describe 'domains/show.haml' do
+
+  let(:domain) { FactoryGirl.create(:domain, domain: 'Cool Domain', description: 'Awesome') }
+
   before(:each) do
-    @domain = assign(:domain, stub_model(Domain,
-      :domain => "Domain",
-      :description => "MyText"
-    ))
+    @domain = domain
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    expect(rendered).to match(/Domain/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(domain.domain)
+    expect(rendered).to match(domain.description)
   end
+
+  it 'shows link count' do
+    domain.link_count = 33
+    render
+    expect(rendered).to match(/Links: 33/)
+  end
+
 end
