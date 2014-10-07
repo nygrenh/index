1f3126cd72c9cca021529d292e6e241668199314
Henrik Nygren <nygrenh@gmail.com>
2014-10-07 20:30:26 +0300

    Don't show welcome screen to users that are logged in

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
+include LoginHelper
 
-RSpec.describe WelcomeController, :type => :controller do
+RSpec.describe WelcomeController, type: :controller do
 
-  describe "GET index" do
-    it "returns http success" do
+  describe 'GET index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    context 'if logged in in' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:valid_session) { { user_id: user.id } }
+
+      it 'should redirect to links_path' do
+        get :index, {}, valid_session
+        expect(response).to redirect_to(links_path)
+      end
     end
   end
 
