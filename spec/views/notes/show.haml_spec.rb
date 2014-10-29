cc0f7500d71fbcd903d8fde593350c539a1831d9
Henrik Nygren <nygrenh@gmail.com>
2014-10-29 17:34:31 +0200

    Show timestamps on show note page

--- spec/views/notes/show.haml_spec.rb
+++ spec/views/notes/show.haml_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 describe "notes/show.haml" do
+  let(:note) { FactoryGirl.create(:note, name: 'Some random name', text: 'The text') }
   before(:each) do
-    @note = assign(:note, stub_model(Note,
-      :name => "Name",
-      :text => "MyText",
-      :user_id => ""
-    ))
+    @note = assign(:note, note)
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(note.name)
+    expect(rendered).to match(note.text)
     expect(rendered).to match(//)
+  end
+
+  it 'shows timestamp' do
+    render
+    expect(rendered).to match(/just now/)
   end
 end
