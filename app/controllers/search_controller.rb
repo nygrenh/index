5881e12ef8f8dfdd161748f4ccae84458657e9ef
Henrik Nygren <nygrenh@gmail.com>
2015-01-11 21:21:24 +0200

    Paginate search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,10 @@ class SearchController < ApplicationController
   before_action :ensure_that_signed_in
 
   def index
-    @results = current_user.links.search(params[:q])
+    @results = current_user.links
+               .search(params[:q])
+               .includes(:tags, :domain, :user)
+               .page(params[:page])
+               .load
   end
 end
