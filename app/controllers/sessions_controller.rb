48028c6bca804515e2dd028d91638f0480a1b57e
Henrik Nygren <nygrenh@gmail.com>
2015-03-31 20:41:58 +0300

    Refactor session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,28 @@
 class SessionsController < ApplicationController
+  before_action :set_user, only: :create
+
   def new
   end
 
   def create
-    name = params[:name]
-    user = User.find_by('lower(name) = ?', name.downcase)
-    if user.nil? || !user.authenticate(params[:password])
-      redirect_to :back, alert: 'Username and password do not match.'
-    else
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to session[:return_to] || links_path, notice: "You've logged in."
       session[:return_to] = nil
+    else
+      redirect_to :back, alert: 'Username and password do not match.'
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to :root, notice: "You've logged out."
+  end
+
+  private
+
+  def set_user
+    name = params[:name]
+    @user = User.find_by('lower(name) = ?', name.downcase)
   end
 end
