e4aed92e1660e22f8f7848d1441f7ad1ab558d9d
Henrik Nygren <nygrenh@gmail.com>
2015-04-03 23:14:50 +0300

    Refactor timestamp logic

--- app/models/concerns/timestamped.rb
+++ app/models/concerns/timestamped.rb
@@ -4,16 +4,36 @@ module Timestamped
 
   def timestamp
     diff = Time.now.to_i - created_at.to_i
-    case
-    when diff >= 1.year then created_at.strftime('%B %-d %Y')
-    when diff >= 1.day then created_at.strftime('%B %-d')
-    when diff >= 1.hour then time_s(diff, 1.hour, 'hour')
-    when diff >= 1.minute then time_s(diff, 1.minute, 'minute')
-    else 'just now'
+    case diff
+    when under(1.minute) then 'just now'
+    when under(1.hour) then minutes_ago(diff)
+    when under(1.day) then hours_ago(diff)
+    when under(1.year) then date_and_month
+    else full_date
     end
   end
 
   private
+
+  def under(time)
+    Range.new(0, time - 1.second)
+  end
+
+  def date_and_month
+    created_at.strftime('%B %-d')
+  end
+
+  def full_date
+    created_at.strftime('%B %-d %Y')
+  end
+
+  def minutes_ago(diff)
+    time_s(diff, 1.minute, 'minute')
+  end
+
+  def hours_ago(diff)
+    time_s(diff, 1.hour, 'hour')
+  end
 
   def time_s(diff, divisor, word)
     pluralize(diff / divisor, word) + ' ago'
