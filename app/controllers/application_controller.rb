4e019b0b21ed84bb015b7b7bf8af7d6d639e9916
Henrik Nygren <nygrenh@gmail.com>
2015-04-06 23:56:57 +0300

    Refactor current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    return nil if session[:user_id].nil?
+    return nil unless session[:user_id]
     User.find_by id: session[:user_id]
   end
 
